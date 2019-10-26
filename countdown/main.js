be006c5adf0354ffd6adbb40ae2b81174b815339
jasi-dev <jasminhouben@gmail.com>
2019-10-26 22:13:44 +0200

    adding sketch files

---
+++ countdown/main.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+
+let startNumber = 1;
+console.log('start nummer:', startNumber);
+
+function increment() {
+    startNumber = startNumber + 1;
+    console.log('start nummer:', startNumber);
+}
+
+function decrement() {
+    startNumber = startNumber - 1;
+    console.log('start nummer:', startNumber);
+}
